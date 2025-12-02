8660fc1c0524e04649a779f22ad4d403c9d1bef1
Sergey25081997 <sergeimolodtsov25081997@gmail.com>
2025-12-02 16:14:14 +0300

    Initial commit: React list app

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Конфигурация Vite - инструмента для быстрой разработки React приложений
+export default defineConfig({
+  plugins: [react()],
+})
